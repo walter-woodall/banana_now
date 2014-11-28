721b2fde53b6f86d3d1af290d0994bfb31a74f6f
walter-woodall <walter.woodall.iv@gmail.com>
2014-11-28 17:56:59 -0500

    test test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Hellow World! Your App is working"));
+        return ok(index.render("Hellow World! Your App is working.Boom Done!"));
     }
 
 }
